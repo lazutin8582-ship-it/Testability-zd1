e7c2cdce878c072d03a44225ae43968dac2b7d38
alex la <lazutin@lenta.ru>
2026-05-01 18:43:31 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int  rublePerMile = 20;
+        int tiketCost = 13676;
+        int bonusMile = tiketCost / rublePerMile;
+        System.out.println(bonusMile);
+
+    }
+}
